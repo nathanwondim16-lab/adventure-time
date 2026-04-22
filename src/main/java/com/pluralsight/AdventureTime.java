c2e8a87fe6ccf77c64c3de1fc6d9f151d3bdd57e
Nathan Wondim <nathanwondim16@gmail.com>
2026-04-22 11:16:50 -0700

    Added loadAdventure() method

--- src/main/java/com/pluralsight/AdventureTime.java
+++ src/main/java/com/pluralsight/AdventureTime.java
@@ -1,7 +1,22 @@
 package com.pluralsight;
 
+import java.io.*;
+import java.util.*;
+
 public class AdventureTime {
     static void main(String[] args) {
-        
+        loadAdventure();
+    }
+
+    public static void loadAdventure() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("adventure1.csv"));
+            String line = reader.readLine(); // Skips first line
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Something went wrong");
+        }
     }
 }
